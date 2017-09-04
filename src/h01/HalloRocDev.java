a4c7bd187f285b2b0067b81eb5f9d73a663d81c4
MrTim <trijsenbrij@roc-dev.com>
2017-09-04 10:53:03 +0200

    kappalmao

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -5,7 +5,6 @@ package h01;
  */
 public class HalloRocDev {
 
-    public static void main(String[] args) {
-        System.out.println("Hallo klas 7A!");
+    public static void main(String[] args) {System.out.println("Hallo klas 7A!");
     }
 }
